287208db1e8341c0d54ddbe02697bab45a971c62
inamuu <8310973+inamuu@users.noreply.github.com>
2021-02-26 23:55:50 +0900

    Add slack function

---
+++ slack/notify.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+)
+
+type payload struct {
+	Text string `json:"text"`
+}
+
+func slackNotify(webhookUrl string) (err error) {
+	jsonP, err := json.Marshal(payload{Text: "Hello, World!"})
+	if err != nil {
+		return err
+	}
+
+	resp, err := http.PostForm(
+		webhookUrl,
+		url.Values{
+			"payload": {string(jsonP)}})
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	return
+}
+
+func main() {
+	webhookUrl := ""
+	err := slackNotify(webhookUrl)
+	if err != nil {
+		return
+	}
+	fmt.Println("ok")
+}
