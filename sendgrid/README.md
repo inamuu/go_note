3d1490cbeed573ab038d43777e0c73832a498584
inamuu <8310973+inamuu@users.noreply.github.com>
2021-03-01 23:58:02 +0900

    Implement a function add a contact to a list

--- sendgrid/README.md
+++ sendgrid/README.md
@@ -5,11 +5,15 @@ I want to add a contact to my contact list of sendgrid marketing campaign.
 
 ref: https://github.com/sendgrid/sendgrid-go#processing-inbound-email
 
+To add a contact using 
+
+ref: https://sendgrid.com/docs/api-reference/
+
 ## How to use
 
 1. Get API key from SendGrid Web Console.
 2. Run below command
 
 ```sh
-SENDGRID_API_KEY=SG.XXXXX go run ./sendgrid.go
+SENDGRID_API_KEY=SG.XXXXX LIST_ID=XXX EMAIL=XXX go run ./sendgrid.go
 ```
--- sendgrid/sendgrid.go
+++ sendgrid/sendgrid.go
@@ -2,22 +2,45 @@ package main
 
 import (
 	"fmt"
-	"log"
+	"io/ioutil"
+	"net/http"
 	"os"
-
-	"github.com/sendgrid/sendgrid-go"
+	"strings"
 )
 
 func main() {
-	request := sendgrid.GetRequest(os.Getenv("SENDGRID_API_KEY"), "/v3/api_keys", "https://api.sendgrid.com")
-	request.Method = "GET"
 
-	response, err := sendgrid.API(request)
-	if err != nil {
-		log.Println(err)
-	} else {
-		fmt.Println(response.StatusCode)
-		fmt.Println(response.Body)
-		fmt.Println(response.Headers)
+	url := "https://api.sendgrid.com/v3/marketing/contacts"
+	apiKey := os.Getenv("SENDGRID_API_KEY")
+	list_id := os.Getenv("LIST_ID")
+	email := os.Getenv("EMAIL")
+
+	if apiKey == "" {
+		fmt.Println("Set API Key")
+		return
 	}
+
+	if list_id == "" {
+		fmt.Println("Set List Id")
+		return
+	}
+
+	payload := strings.NewReader(fmt.Sprintf(`{
+		"list_ids":["%s"],
+		"contacts":[{"email":"%s"}]
+		}`, list_id, email))
+
+	req, _ := http.NewRequest("PUT", url, payload)
+
+	req.Header.Add("authorization", "Bearer "+fmt.Sprintf(apiKey))
+	req.Header.Add("content-type", "application/json")
+
+	res, _ := http.DefaultClient.Do(req)
+
+	defer res.Body.Close()
+	body, _ := ioutil.ReadAll(res.Body)
+
+	fmt.Println(res)
+	fmt.Println(string(body))
+
 }
