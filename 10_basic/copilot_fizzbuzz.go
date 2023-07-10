3b86d3105e166c9ef6db10972ec3530f9278cf63
inamuu <8310973+inamuu@users.noreply.github.com>
2023-07-10 16:57:30 +0900

    tidy up directories with sorting alfabet

---
+++ 10_basic/copilot_fizzbuzz.go
@@ -0,0 +1,16 @@
+//fizzbuzz
+package main
+
+func main {
+	for i := 1; i <= 100; i++ {
+		if i%15 == 0 {
+			println("FizzBuzz")
+		} else if i%5 == 0 {
+			println("Buzz")
+		} else if i%3 == 0 {
+			println("Fizz")
+		} else {
+			println(i)
+		}
+	}
+}
